e437687eb3d4a095e4954184328af5241105b8d5
samir20-23 <aouladamarsamir@gmail.com>
2025-06-28 20:49:37 +0100

    fix(UI):pages homes , components react app

--- B_Application/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ B_Application/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51d6af27515554fb6725f4ffcab6aca065ab2116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73cafbe487658cc7265c960df84e92956bffa965")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
